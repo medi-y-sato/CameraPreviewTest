0fdc5cdc0dcdaccf9c98cd7cf947e2acab7534f0
Yoshiaki Satoh <y-sato@mediba.jp>
2017-05-09 15:22:07 +0900

    吹き飛ばない状況を作れた、復帰時にCameraPreviewのステータスを呼んでプラグインがAvailableかどうかをチェックする

--- src/pages/camera-preview/camera-preview.ts
+++ src/pages/camera-preview/camera-preview.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, Platform } from 'ionic-angular';
 
 import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions } from '@ionic-native/camera-preview';
 
@@ -12,6 +12,7 @@ export class CameraPreviewPage {
 
   picture: string
   pictureOpts: CameraPreviewPictureOptions
+  cameraPreviewOpts: CameraPreviewOptions
   tabBarElement: any
 
   constructor(
@@ -19,8 +20,67 @@ export class CameraPreviewPage {
     public navParams: NavParams,
     private cameraPreview: CameraPreview,
     private viewController: ViewController,
+    private platform: Platform,
   ) {
     this.tabBarElement = document.querySelector('.tabbar.show-tabbar')
+
+    this.cameraPreviewOpts = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'rear',
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: true,
+      alpha: 1
+    }
+
+    this.platform.resume.subscribe(
+      res => {
+        console.log("this.platform.resume res")
+
+        let loopCounter = 0
+        let loopId = setInterval(() => {
+          loopCounter++
+
+          this.cameraPreview.getSupportedFlashModes()
+            .then((res) => {
+              if (typeof res !== "undefined") {
+                clearInterval(loopId)
+
+                this.cameraPreview.stopCamera()
+                  .then(() => {
+                    this.cameraPreview.startCamera(this.cameraPreviewOpts)
+                      .then(() => {})
+                      .catch(error => console.error(error))
+                  })
+                  .catch(error => console.error(error))
+              }
+            })
+            .catch((error) => console.error(error))
+
+          if (loopCounter >= 3) {
+            clearInterval(loopId)
+            this.startCamera()
+          }
+
+        }, 500);
+
+      },
+      error => console.error(error),
+      () => { }
+    )
+
+    this.platform.pause.subscribe(
+      res => {
+        console.log("this.platform.pause res")
+        this.stopCamera()
+      },
+      error => console.error(error),
+      () => { }
+    )
+
   }
 
   ionViewWillEnter() {
@@ -40,20 +100,10 @@ export class CameraPreviewPage {
 
 
   private startCamera() {
-    const cameraPreviewOpts: CameraPreviewOptions = {
-      x: 0,
-      y: 0,
-      width: window.screen.width,
-      height: window.screen.height,
-      camera: 'rear',
-      tapPhoto: true,
-      previewDrag: true,
-      toBack: true,
-      alpha: 1
-    };
+    console.log("startCamera()")
 
     // start camera
-    this.cameraPreview.startCamera(cameraPreviewOpts).then(
+    this.cameraPreview.startCamera(this.cameraPreviewOpts).then(
       (res) => {
         console.log(res)
       },
@@ -63,6 +113,8 @@ export class CameraPreviewPage {
   }
 
   private stopCamera() {
+    console.log("stopCamera()")
+
     this.cameraPreview.stopCamera().then(
       (res) => {
         console.log(res)
@@ -86,6 +138,8 @@ export class CameraPreviewPage {
       console.log(err);
       this.picture = 'assets/img/test.jpg';
     });
+
+
 
   }
 
